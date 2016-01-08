e417e6283b360fbc9471ddc37b89ab2a8fcfd968
Jonas Jensen <jj@issuu.com>
2016-01-08 14:09:14 +0100

    Regenerate OCaml code from Coq 8.4pl6

--- lib/core/msgpackCore.mli
+++ lib/core/msgpackCore.mli
@@ -1,3 +1,7 @@
+type __ = Obj.t
+
+val negb : bool -> bool
+
 val fst : ('a1 * 'a2) -> 'a1
 
 val snd : ('a1 * 'a2) -> 'a2
@@ -6,51 +10,576 @@ val length : 'a1 list -> int
 
 val app : 'a1 list -> 'a1 list -> 'a1 list
 
+type comparison =
+| Eq
+| Lt
+| Gt
+
+type compareSpecT =
+| CompEqT
+| CompLtT
+| CompGtT
+
+val compareSpec2Type : comparison -> compareSpecT
+
+type 'a compSpecT = compareSpecT
+
+val compSpec2Type : 'a1 -> 'a1 -> comparison -> 'a1 compSpecT
+
+type 'a sig0 =
+  'a
+  (* singleton inductive, whose constructor was exist *)
+
 val plus : int -> int -> int
 
 val mult : int -> int -> int
 
+val nat_iter : int -> ('a1 -> 'a1) -> 'a1 -> 'a1
+
 type positive =
-  | XI of positive
-  | XO of positive
-  | XH
-
-val psucc : positive -> positive
-
-val pplus : positive -> positive -> positive
-
-val pplus_carry : positive -> positive -> positive
-
-val pmult_nat : positive -> int -> int
-
-val nat_of_P : positive -> int
-
-val p_of_succ_nat : int -> positive
-
-val pmult : positive -> positive -> positive
+| XI of positive
+| XO of positive
+| XH
 
 type n =
-  | N0
-  | Npos of positive
+| N0
+| Npos of positive
 
-val nplus : n -> n -> n
+type reflect =
+| ReflectT
+| ReflectF
 
-val nmult : n -> n -> n
+val iff_reflect : bool -> reflect
 
-val nat_of_N : n -> int
+module Pos : 
+ sig 
+  type t = positive
+  
+  val succ : positive -> positive
+  
+  val add : positive -> positive -> positive
+  
+  val add_carry : positive -> positive -> positive
+  
+  val pred_double : positive -> positive
+  
+  val pred : positive -> positive
+  
+  val pred_N : positive -> n
+  
+  type mask =
+  | IsNul
+  | IsPos of positive
+  | IsNeg
+  
+  val mask_rect : 'a1 -> (positive -> 'a1) -> 'a1 -> mask -> 'a1
+  
+  val mask_rec : 'a1 -> (positive -> 'a1) -> 'a1 -> mask -> 'a1
+  
+  val succ_double_mask : mask -> mask
+  
+  val double_mask : mask -> mask
+  
+  val double_pred_mask : positive -> mask
+  
+  val pred_mask : mask -> mask
+  
+  val sub_mask : positive -> positive -> mask
+  
+  val sub_mask_carry : positive -> positive -> mask
+  
+  val sub : positive -> positive -> positive
+  
+  val mul : positive -> positive -> positive
+  
+  val iter : positive -> ('a1 -> 'a1) -> 'a1 -> 'a1
+  
+  val pow : positive -> positive -> positive
+  
+  val square : positive -> positive
+  
+  val div2 : positive -> positive
+  
+  val div2_up : positive -> positive
+  
+  val size_nat : positive -> int
+  
+  val size : positive -> positive
+  
+  val compare_cont : positive -> positive -> comparison -> comparison
+  
+  val compare : positive -> positive -> comparison
+  
+  val min : positive -> positive -> positive
+  
+  val max : positive -> positive -> positive
+  
+  val eqb : positive -> positive -> bool
+  
+  val leb : positive -> positive -> bool
+  
+  val ltb : positive -> positive -> bool
+  
+  val sqrtrem_step :
+    (positive -> positive) -> (positive -> positive) -> (positive * mask) ->
+    positive * mask
+  
+  val sqrtrem : positive -> positive * mask
+  
+  val sqrt : positive -> positive
+  
+  val gcdn : int -> positive -> positive -> positive
+  
+  val gcd : positive -> positive -> positive
+  
+  val ggcdn : int -> positive -> positive -> positive * (positive * positive)
+  
+  val ggcd : positive -> positive -> positive * (positive * positive)
+  
+  val coq_Nsucc_double : n -> n
+  
+  val coq_Ndouble : n -> n
+  
+  val coq_lor : positive -> positive -> positive
+  
+  val coq_land : positive -> positive -> n
+  
+  val ldiff : positive -> positive -> n
+  
+  val coq_lxor : positive -> positive -> n
+  
+  val shiftl_nat : positive -> int -> positive
+  
+  val shiftr_nat : positive -> int -> positive
+  
+  val shiftl : positive -> n -> positive
+  
+  val shiftr : positive -> n -> positive
+  
+  val testbit_nat : positive -> int -> bool
+  
+  val testbit : positive -> n -> bool
+  
+  val iter_op : ('a1 -> 'a1 -> 'a1) -> positive -> 'a1 -> 'a1
+  
+  val to_nat : positive -> int
+  
+  val of_nat : int -> positive
+  
+  val of_succ_nat : int -> positive
+ end
 
-val n_of_nat : int -> n
+module Coq_Pos : 
+ sig 
+  type t = positive
+  
+  val succ : positive -> positive
+  
+  val add : positive -> positive -> positive
+  
+  val add_carry : positive -> positive -> positive
+  
+  val pred_double : positive -> positive
+  
+  val pred : positive -> positive
+  
+  val pred_N : positive -> n
+  
+  type mask = Pos.mask =
+  | IsNul
+  | IsPos of positive
+  | IsNeg
+  
+  val mask_rect : 'a1 -> (positive -> 'a1) -> 'a1 -> mask -> 'a1
+  
+  val mask_rec : 'a1 -> (positive -> 'a1) -> 'a1 -> mask -> 'a1
+  
+  val succ_double_mask : mask -> mask
+  
+  val double_mask : mask -> mask
+  
+  val double_pred_mask : positive -> mask
+  
+  val pred_mask : mask -> mask
+  
+  val sub_mask : positive -> positive -> mask
+  
+  val sub_mask_carry : positive -> positive -> mask
+  
+  val sub : positive -> positive -> positive
+  
+  val mul : positive -> positive -> positive
+  
+  val iter : positive -> ('a1 -> 'a1) -> 'a1 -> 'a1
+  
+  val pow : positive -> positive -> positive
+  
+  val square : positive -> positive
+  
+  val div2 : positive -> positive
+  
+  val div2_up : positive -> positive
+  
+  val size_nat : positive -> int
+  
+  val size : positive -> positive
+  
+  val compare_cont : positive -> positive -> comparison -> comparison
+  
+  val compare : positive -> positive -> comparison
+  
+  val min : positive -> positive -> positive
+  
+  val max : positive -> positive -> positive
+  
+  val eqb : positive -> positive -> bool
+  
+  val leb : positive -> positive -> bool
+  
+  val ltb : positive -> positive -> bool
+  
+  val sqrtrem_step :
+    (positive -> positive) -> (positive -> positive) -> (positive * mask) ->
+    positive * mask
+  
+  val sqrtrem : positive -> positive * mask
+  
+  val sqrt : positive -> positive
+  
+  val gcdn : int -> positive -> positive -> positive
+  
+  val gcd : positive -> positive -> positive
+  
+  val ggcdn : int -> positive -> positive -> positive * (positive * positive)
+  
+  val ggcd : positive -> positive -> positive * (positive * positive)
+  
+  val coq_Nsucc_double : n -> n
+  
+  val coq_Ndouble : n -> n
+  
+  val coq_lor : positive -> positive -> positive
+  
+  val coq_land : positive -> positive -> n
+  
+  val ldiff : positive -> positive -> n
+  
+  val coq_lxor : positive -> positive -> n
+  
+  val shiftl_nat : positive -> int -> positive
+  
+  val shiftr_nat : positive -> int -> positive
+  
+  val shiftl : positive -> n -> positive
+  
+  val shiftr : positive -> n -> positive
+  
+  val testbit_nat : positive -> int -> bool
+  
+  val testbit : positive -> n -> bool
+  
+  val iter_op : ('a1 -> 'a1 -> 'a1) -> positive -> 'a1 -> 'a1
+  
+  val to_nat : positive -> int
+  
+  val of_nat : int -> positive
+  
+  val of_succ_nat : int -> positive
+  
+  val eq_dec : positive -> positive -> bool
+  
+  val peano_rect : 'a1 -> (positive -> 'a1 -> 'a1) -> positive -> 'a1
+  
+  val peano_rec : 'a1 -> (positive -> 'a1 -> 'a1) -> positive -> 'a1
+  
+  type coq_PeanoView =
+  | PeanoOne
+  | PeanoSucc of positive * coq_PeanoView
+  
+  val coq_PeanoView_rect :
+    'a1 -> (positive -> coq_PeanoView -> 'a1 -> 'a1) -> positive ->
+    coq_PeanoView -> 'a1
+  
+  val coq_PeanoView_rec :
+    'a1 -> (positive -> coq_PeanoView -> 'a1 -> 'a1) -> positive ->
+    coq_PeanoView -> 'a1
+  
+  val peanoView_xO : positive -> coq_PeanoView -> coq_PeanoView
+  
+  val peanoView_xI : positive -> coq_PeanoView -> coq_PeanoView
+  
+  val peanoView : positive -> coq_PeanoView
+  
+  val coq_PeanoView_iter :
+    'a1 -> (positive -> 'a1 -> 'a1) -> positive -> coq_PeanoView -> 'a1
+  
+  val eqb_spec : positive -> positive -> reflect
+  
+  val switch_Eq : comparison -> comparison -> comparison
+  
+  val mask2cmp : mask -> comparison
+  
+  val leb_spec0 : positive -> positive -> reflect
+  
+  val ltb_spec0 : positive -> positive -> reflect
+  
+  module Private_Tac : 
+   sig 
+    
+   end
+  
+  module Private_Dec : 
+   sig 
+    val max_case_strong :
+      positive -> positive -> (positive -> positive -> __ -> 'a1 -> 'a1) ->
+      (__ -> 'a1) -> (__ -> 'a1) -> 'a1
+    
+    val max_case :
+      positive -> positive -> (positive -> positive -> __ -> 'a1 -> 'a1) ->
+      'a1 -> 'a1 -> 'a1
+    
+    val max_dec : positive -> positive -> bool
+    
+    val min_case_strong :
+      positive -> positive -> (positive -> positive -> __ -> 'a1 -> 'a1) ->
+      (__ -> 'a1) -> (__ -> 'a1) -> 'a1
+    
+    val min_case :
+      positive -> positive -> (positive -> positive -> __ -> 'a1 -> 'a1) ->
+      'a1 -> 'a1 -> 'a1
+    
+    val min_dec : positive -> positive -> bool
+   end
+  
+  val max_case_strong :
+    positive -> positive -> (__ -> 'a1) -> (__ -> 'a1) -> 'a1
+  
+  val max_case : positive -> positive -> 'a1 -> 'a1 -> 'a1
+  
+  val max_dec : positive -> positive -> bool
+  
+  val min_case_strong :
+    positive -> positive -> (__ -> 'a1) -> (__ -> 'a1) -> 'a1
+  
+  val min_case : positive -> positive -> 'a1 -> 'a1 -> 'a1
+  
+  val min_dec : positive -> positive -> bool
+ end
+
+module N : 
+ sig 
+  type t = n
+  
+  val zero : n
+  
+  val one : n
+  
+  val two : n
+  
+  val succ_double : n -> n
+  
+  val double : n -> n
+  
+  val succ : n -> n
+  
+  val pred : n -> n
+  
+  val succ_pos : n -> positive
+  
+  val add : n -> n -> n
+  
+  val sub : n -> n -> n
+  
+  val mul : n -> n -> n
+  
+  val compare : n -> n -> comparison
+  
+  val eqb : n -> n -> bool
+  
+  val leb : n -> n -> bool
+  
+  val ltb : n -> n -> bool
+  
+  val min : n -> n -> n
+  
+  val max : n -> n -> n
+  
+  val div2 : n -> n
+  
+  val even : n -> bool
+  
+  val odd : n -> bool
+  
+  val pow : n -> n -> n
+  
+  val square : n -> n
+  
+  val log2 : n -> n
+  
+  val size : n -> n
+  
+  val size_nat : n -> int
+  
+  val pos_div_eucl : positive -> n -> n * n
+  
+  val div_eucl : n -> n -> n * n
+  
+  val div : n -> n -> n
+  
+  val modulo : n -> n -> n
+  
+  val gcd : n -> n -> n
+  
+  val ggcd : n -> n -> n * (n * n)
+  
+  val sqrtrem : n -> n * n
+  
+  val sqrt : n -> n
+  
+  val coq_lor : n -> n -> n
+  
+  val coq_land : n -> n -> n
+  
+  val ldiff : n -> n -> n
+  
+  val coq_lxor : n -> n -> n
+  
+  val shiftl_nat : n -> int -> n
+  
+  val shiftr_nat : n -> int -> n
+  
+  val shiftl : n -> n -> n
+  
+  val shiftr : n -> n -> n
+  
+  val testbit_nat : n -> int -> bool
+  
+  val testbit : n -> n -> bool
+  
+  val to_nat : n -> int
+  
+  val of_nat : int -> n
+  
+  val iter : n -> ('a1 -> 'a1) -> 'a1 -> 'a1
+  
+  val eq_dec : n -> n -> bool
+  
+  val discr : n -> positive option
+  
+  val binary_rect : 'a1 -> (n -> 'a1 -> 'a1) -> (n -> 'a1 -> 'a1) -> n -> 'a1
+  
+  val binary_rec : 'a1 -> (n -> 'a1 -> 'a1) -> (n -> 'a1 -> 'a1) -> n -> 'a1
+  
+  val peano_rect : 'a1 -> (n -> 'a1 -> 'a1) -> n -> 'a1
+  
+  val peano_rec : 'a1 -> (n -> 'a1 -> 'a1) -> n -> 'a1
+  
+  val leb_spec0 : n -> n -> reflect
+  
+  val ltb_spec0 : n -> n -> reflect
+  
+  module Private_BootStrap : 
+   sig 
+    
+   end
+  
+  val recursion : 'a1 -> (n -> 'a1 -> 'a1) -> n -> 'a1
+  
+  module Private_OrderTac : 
+   sig 
+    module IsTotal : 
+     sig 
+      
+     end
+    
+    module Tac : 
+     sig 
+      
+     end
+   end
+  
+  module Private_NZPow : 
+   sig 
+    
+   end
+  
+  module Private_NZSqrt : 
+   sig 
+    
+   end
+  
+  val sqrt_up : n -> n
+  
+  val log2_up : n -> n
+  
+  module Private_NZDiv : 
+   sig 
+    
+   end
+  
+  val lcm : n -> n -> n
+  
+  val eqb_spec : n -> n -> reflect
+  
+  val b2n : bool -> n
+  
+  val setbit : n -> n -> n
+  
+  val clearbit : n -> n -> n
+  
+  val ones : n -> n
+  
+  val lnot : n -> n -> n
+  
+  module Private_Tac : 
+   sig 
+    
+   end
+  
+  module Private_Dec : 
+   sig 
+    val max_case_strong :
+      n -> n -> (n -> n -> __ -> 'a1 -> 'a1) -> (__ -> 'a1) -> (__ -> 'a1) ->
+      'a1
+    
+    val max_case :
+      n -> n -> (n -> n -> __ -> 'a1 -> 'a1) -> 'a1 -> 'a1 -> 'a1
+    
+    val max_dec : n -> n -> bool
+    
+    val min_case_strong :
+      n -> n -> (n -> n -> __ -> 'a1 -> 'a1) -> (__ -> 'a1) -> (__ -> 'a1) ->
+      'a1
+    
+    val min_case :
+      n -> n -> (n -> n -> __ -> 'a1 -> 'a1) -> 'a1 -> 'a1 -> 'a1
+    
+    val min_dec : n -> n -> bool
+   end
+  
+  val max_case_strong : n -> n -> (__ -> 'a1) -> (__ -> 'a1) -> 'a1
+  
+  val max_case : n -> n -> 'a1 -> 'a1 -> 'a1
+  
+  val max_dec : n -> n -> bool
+  
+  val min_case_strong : n -> n -> (__ -> 'a1) -> (__ -> 'a1) -> 'a1
+  
+  val min_case : n -> n -> 'a1 -> 'a1 -> 'a1
+  
+  val min_dec : n -> n -> bool
+ end
 
 val flat_map : ('a1 -> 'a2 list) -> 'a1 list -> 'a2 list
 
 val eucl_dev : int -> int -> (int * int)
 
 type ascii =
-  | Ascii of bool * bool * bool * bool * bool * bool * bool * bool
+| Ascii of bool * bool * bool * bool * bool * bool * bool * bool
 
-val zero : ascii
+val zero0 : ascii
 
-val one : ascii
+val one0 : ascii
 
 val shift : bool -> ascii -> ascii
 
@@ -74,7 +603,7 @@ val split_at : int -> 'a1 list -> 'a1 list * 'a1 list
 
 val pair : 'a1 list -> ('a1 * 'a1) list
 
-val pow : int -> int
+val pow0 : int -> int
 
 val divmod : int -> int -> (int * int)
 
@@ -107,29 +636,29 @@ val list_of_ascii32 : ascii32 -> ascii8 list
 val list_of_ascii64 : ascii64 -> ascii8 list
 
 type object0 =
-  | Bool of bool
-  | Nil
-  | PFixnum of ascii8
-  | NFixnum of ascii8
-  | Uint8 of ascii8
-  | Uint16 of ascii16
-  | Uint32 of ascii32
-  | Uint64 of ascii64
-  | Int8 of ascii8
-  | Int16 of ascii16
-  | Int32 of ascii32
-  | Int64 of ascii64
-  | Float of ascii32
-  | Double of ascii64
-  | FixRaw of ascii8 list
-  | Raw16 of ascii8 list
-  | Raw32 of ascii8 list
-  | FixArray of object0 list
-  | Array16 of object0 list
-  | Array32 of object0 list
-  | FixMap of (object0 * object0) list
-  | Map16 of (object0 * object0) list
-  | Map32 of (object0 * object0) list
+| Bool of bool
+| Nil
+| PFixnum of ascii8
+| NFixnum of ascii8
+| Uint8 of ascii8
+| Uint16 of ascii16
+| Uint32 of ascii32
+| Uint64 of ascii64
+| Int8 of ascii8
+| Int16 of ascii16
+| Int32 of ascii32
+| Int64 of ascii64
+| Float of ascii32
+| Double of ascii64
+| FixRaw of ascii8 list
+| Raw16 of ascii8 list
+| Raw32 of ascii8 list
+| FixArray of object0 list
+| Array16 of object0 list
+| Array32 of object0 list
+| FixMap of (object0 * object0) list
+| Map16 of (object0 * object0) list
+| Map32 of (object0 * object0) list
 
 val atat : ('a1 -> 'a2) -> 'a1 -> 'a2
 
